83f56fe88d9563418b5a403fddf598ae09cdf791
sridevijagadesh <sridevi.j2719@gmail.com>
2024-04-04 21:32:15 +0530

    fetching-popular-datajs

--- src/pages/Shoped.jsx
+++ src/pages/Shoped.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../component/Hero/Hero'
+import Popular from '../component/Popular/Popular'
 
 const shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
     </div>
   )
 }
