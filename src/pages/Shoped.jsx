b55566eb645a76e9a0ea9efa00aa80829b1a20f0
sridevijagadesh <sridevi.j2719@gmail.com>
2024-04-04 21:59:45 +0530

    offer-banner

--- src/pages/Shoped.jsx
+++ src/pages/Shoped.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../component/Hero/Hero'
 import Popular from '../component/Popular/Popular'
+import Offer from '../component/Offer/Offer'
 
 const shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offer/>
     </div>
   )
 }
