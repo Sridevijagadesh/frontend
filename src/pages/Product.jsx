1bdbd5196ec96f087396356fb497f4476e5c8aea
sridevijagadesh <sridevi.j2719@gmail.com>
2024-04-08 18:44:30 +0530

    creating breadcrum and  productDisplay

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,15 +2,22 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/shopContext'
 import{useParams} from 'react-router-dom'
 import Breadcrum from '../component/Breadcrums/Breadcrum'
+import ProductDisplay from '../component/ProductDisplay/ProductDisplay'
+// import ProductDisplay from '../component/ProductDisplay/ProductDisplay'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
-  const {productId} = useParams();
-  const product = all_product.find((e)=> e.id === Number(productId));
+  const { productId } = useParams();
+  
+  console.log( 'i am product ' , productId)
+  const product = all_product.find((e)=> e.id === Number( productId ));
+
+  console.log(product)
   return (
     <div>
-       <Breadcrum product = {product}/>
-       vfmnfdk
+ <Breadcrum product ={product}/>
+ <ProductDisplay product = {product} />
+       
     </div>
   )
 }
