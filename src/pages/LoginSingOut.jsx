11ec82208aa6db6a76bf81ee9adb71aaa174ce0c
sridevijagadesh <sridevi.j2719@gmail.com>
2024-05-23 14:44:34 +0530

    loging

--- src/pages/LoginSingOut.jsx
+++ src/pages/LoginSingOut.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/loginsignup.css'
+import Axios from 'axios'
 
 const LoginSingOut = () => {
+  const [name , setName] = useState('');
+  const [email , setEmail] = useState('');
+  const [password , setpassword] = useState('')
+  const myData = ()=>{
+ Axios.post('http://localhost/8081/loginPage',{
+  name:name,
+  email:email,
+  password:password
+ })
+  }
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>sign up</h1>
         <div className="loginsignup-fields">
-          <input type='text' placeholder='your name'></input>
-          <input type='email' placeholder='your email'></input>
-          <input type='password' placeholder='password'></input>
+          <input type='text' placeholder='your name' onChange={(e)=>{setName(e.target.value)}}></input>
+          <input type='email' placeholder='your email'onChange={(e)=>{setEmail(e.target.value)}}></input>
+          <input type='password' placeholder='password' onChange={(e)=>{setpassword(e.target.value)}}></input>
         </div>
         <button>continue</button>
         <div className="loginsignup-login">Alredy have an account? <span>Login here</span></div>
